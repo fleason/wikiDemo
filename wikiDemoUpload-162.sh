a0c9e597e89da1bf2c058f0bcb11ab550d5426e8
fleason <fleason@me.com>
2014-07-18 17:00:21 -0500

    Rename files generic and modify Upload for getopts

--- wikiDemoUpload-162.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script uploads necessary demo files to the demo server
-
-scp -i fleason_me_com.pem wikiDemoScript-162.sh index.php diffconfig-162.patch cloud@209.251.181.162:~/
-
-echo "Now open another Terminal session and log into the demo server with \n"
-echo "ssh -i fleason_me_com.pem cloud@209.251.181.162"
-
-read
